2eeb37d7770efbde662f2d72813b95579e215f94
Mihir Nirvikar <mihirnirvikar3@gmail.com>
2025-09-25 13:40:02 +0530

    Igniting our app using parcel

---
+++ Episode2/dist/Episode2.6bd02f5a.js
@@ -0,0 +1,7 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const heading = React.createElement("h1", {
+    id: "title"
+}, "Hello World from React");
+root.render(heading);
+
+//# sourceMappingURL=Episode2.6bd02f5a.js.map
